5eb0c0798872769db9005651a5891f82401af29c
Jeremy Lane <jeremylanes17@gmail.com>
2025-07-11 17:36:56 -1000

    base notions init

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 jeremy_url = 'https://www.jeremy.berlin/'
-url = 'https://docstring.fr/api/books_to_scrape/index.html'
+docstring_url = 'https://docstring.fr/api/books_to_scrape/index.html'
+url = 'https://books.toscrape.com/'
 
-response = requests.get(url)
-response.raise_for_status()
+# response = requests.get(url)
+# response.raise_for_status()
+#
+# with open('index.html', 'w') as f:
+#     f.write(response.text)
 
-with open('index.html', 'w') as f:
-    f.write(response.text)
+with open('index.html', 'r') as f:
+    f = f.read()
+
+
+soup = BeautifulSoup(f, 'html.parser')
+print(soup.prettify())
