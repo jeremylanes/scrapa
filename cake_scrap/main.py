2ccb6eb6979c5a491ccd62a939254970e5a45495
Jeremy Lane <jeremylanes17@gmail.com>
2025-07-11 16:01:32 -1000

    extrat title

--- cake_scrap/main.py
+++ cake_scrap/main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 
+# reading html file
 with open('SITE_RECETTE/recette.html', 'r', encoding='utf-8') as html_file:
     html_content = html_file.read()
 
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-print(soup)
+h1 = soup.find('h1')
+print(f'Titre de la page : {h1.text}')
