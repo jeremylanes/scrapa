72dc850faf8908b83828bf1ceb7b1a2560dbd34d
Jeremy Lane <jeremylanes17@gmail.com>
2025-07-12 16:21:21 -1000

    getting categories

--- src/home_page_analyse.py
+++ src/home_page_analyse.py
@@ -1,5 +1,8 @@
+from pprint import pprint
+
 import requests
 from bs4 import BeautifulSoup
+
 
 url = 'https://books.toscrape.com/'
 
@@ -8,4 +11,15 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup)
+side_categories = soup.find('div', class_='side_categories')
+
+categories = side_categories.find('ul').find('li').find('ul')
+
+# for category in categories.children:
+#     print(category.name)
+
+   # print(category.text.strip())
+
+names = [category.text.strip() for category in categories.children if category.name]
+
+pprint(names)
