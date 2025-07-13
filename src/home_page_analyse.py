8f2f6e5df9bd1553edc5359ade8424aacd140ad7
Jeremy Lane <jeremylanes17@gmail.com>
2025-07-12 16:37:24 -1000

    get images

--- src/home_page_analyse.py
+++ src/home_page_analyse.py
@@ -6,10 +6,13 @@ from bs4 import BeautifulSoup
 
 url = 'https://books.toscrape.com/'
 
-response = requests.get(url)
+# response = requests.get(url)
+
+with open('index.html', 'r') as f:
+    html = f.read()
 
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
 side_categories = soup.find('div', class_='side_categories')
 
@@ -22,4 +25,11 @@ categories = side_categories.find('ul').find('li').find('ul')
 
 names = [category.text.strip() for category in categories.children if category.name]
 
-pprint(names)
+# pprint(names)
+section = soup.find('section')
+
+images = section.find_all('img')
+
+images_urls = [image.get('src') for image in images]
+
+pprint(images_urls)
